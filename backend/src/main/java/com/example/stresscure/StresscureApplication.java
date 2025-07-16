77d7eabf65c059b9fbd7f32026b694ff79bdf4f3
benjo00 <130617538+benjo00@users.noreply.github.com>
2025-07-16 16:46:54 -0500

    delete unit 1 error

---
+++ backend/src/main/java/com/example/stresscure/StresscureApplication.java
@@ -0,0 +1,13 @@
+package com.example.stresscure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StresscureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StresscureApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/stresscure/StresscureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stresscure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StresscureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
