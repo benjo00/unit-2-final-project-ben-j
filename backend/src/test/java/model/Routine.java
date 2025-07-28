49843715cef617e10d3291ec00659fd34afd4d65
benjo00 <130617538+benjo00@users.noreply.github.com>
2025-07-28 16:31:32 -0500

    getters n setters add

--- backend/src/test/java/model/Routine.java
+++ backend/src/test/java/model/Routine.java
@@ -6,15 +6,28 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Routine {
+public class Routine
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String name;
-
-    private String description;
+    private String activity;
+    private String duration;
 
 
-}
+    public Routine() {
+    }
+
+
+    public Routine(String activity, String duration) {
+        this.activity = activity;
+        this.duration = duration;
+    }
+
+    // getters n setters
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
