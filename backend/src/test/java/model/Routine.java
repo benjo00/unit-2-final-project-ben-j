bd5dc748b81d3d00047572d069f980407e47aa87
benjo00 <130617538+benjo00@users.noreply.github.com>
2025-07-28 16:11:34 -0500

    add annotations etcetera

--- backend/src/test/java/model/Routine.java
+++ backend/src/test/java/model/Routine.java
@@ -1,4 +1,20 @@
 package model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Routine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+
 }
