a2fe35e2723962b0ec43cb22595f55edb5db9b28
benjo00 <130617538+benjo00@users.noreply.github.com>
2025-07-29 09:15:12 -0500

    Add missing curly brace and fix typo

--- backend/src/test/java/model/Routine.java
+++ backend/src/test/java/model/Routine.java
@@ -6,51 +6,54 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Routine
+public class Routine {
 
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Long id;
-private String activity;
-private String duration;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String activity;
+    private String duration;
 
 
-public Routine() {
+    public Routine() {
+    }
+
+    public Routine(String activity, String duration) {
+        this.activity = activity;
+        this.duration = duration;
+    }
+
+    // Getters n setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Routine{" +
+                "id=" + id +
+                ", activity='" + activity + '\'' +
+                ", duration='" + duration + '\'' +
+                '}';
+    }
 }
-
-
-public Routine(String activity, String duration) {
-    this.activity = activity;
-    this.duration = duration;
-}
-
-// getters n setters
-
-
-public  setId(Long id) {
-    this.id = id;
-}
-
-public String getActivity() {
-    return activity;
-}
-
-public void setActivity(String activity) {
-    this.activity = activity;
-}
-
-public String getDuration() {
-    return duration;
-}
-
-public void setDuration(String duration) {
-    this.duration = duration;
-}
-
-@Override
-public String toString() {
-    return "Routine{" +
-            "id=" + id +
-            ", activity='" + activity + '\'' +
-            ", duration='" + duration + '\'' +
-            '}';
