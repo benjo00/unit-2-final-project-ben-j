a02cbe184e30f3700886ec5fb443303e39d757d7
benjo00 <130617538+benjo00@users.noreply.github.com>
2025-08-01 14:26:33 -0500

    boilerplate and injecting repository

--- backend/src/test/java/com/example/stresscure/RoutineService.java
+++ backend/src/test/java/com/example/stresscure/RoutineService.java
@@ -1,4 +1,17 @@
-package com.example.stresscure;
+package service;
 
+import model.Routine;
+import repository.RoutineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RoutineService {
+
+    @Autowired
+    private RoutineRepository routineRepository;
+
+    //injecting the repository
 }
