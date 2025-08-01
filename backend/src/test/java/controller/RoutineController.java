0a11cc1bf2086fa492ea513276e6fb321df37233
benjo00 <130617538+benjo00@users.noreply.github.com>
2025-08-01 11:46:43 -0500

    add basic RoutineController with repo injection

--- backend/src/test/java/controller/RoutineController.java
+++ backend/src/test/java/controller/RoutineController.java
@@ -1,6 +1,9 @@
+package controller;
+
 import model.Routine;
 import repository.RoutineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RoutineController {
@@ -8,5 +11,5 @@ public class RoutineController {
     @Autowired
     private RoutineRepository routineRepository;
 
-    // just injected the repo so it can be used
+    // just injected the repo so we can use it
 }
