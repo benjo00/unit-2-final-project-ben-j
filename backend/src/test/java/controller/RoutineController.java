c32e568891eced9dd6bced887aca85b8107ebbd2
benjo00 <130617538+benjo00@users.noreply.github.com>
2025-08-01 11:50:38 -0500

    endpiont

--- backend/src/test/java/controller/RoutineController.java
+++ backend/src/test/java/controller/RoutineController.java
@@ -3,7 +3,10 @@ package controller;
 import model.Routine;
 import repository.RoutineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class RoutineController {
@@ -12,4 +15,10 @@ public class RoutineController {
     private RoutineRepository routineRepository;
 
     // just injected the repo so we can use it
+
+    // basic get endpoint to return all routines
+    @GetMapping("/routines")
+    public List<Routine> getAllRoutines() {
+        return routineRepository.findAll();
+    }
 }
