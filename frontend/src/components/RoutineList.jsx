fdadff5698423037f2a82b103ca04fe64589bdbf
benjo00 <130617538+benjo00@users.noreply.github.com>
2025-08-03 12:46:20 -0500

    add fetch to RoutineList component

--- frontend/src/components/RoutineList.jsx
+++ frontend/src/components/RoutineList.jsx
@@ -1,13 +1,29 @@
 // RoutineList.jsx
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-// for now just show a placeholder. will add more once backend connects
+// basic fetch setup for routines from backend
 
 function RoutineList() {
+  const [routines, setRoutines] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/routines")
+      .then((res) => res.json())
+      .then((data) => {
+        setRoutines(data);
+      });
+  }, []);
+
   return (
     <div>
       <h2>Routine List</h2>
-      <p>this will eventually show all routines from backend</p>
+      <ul>
+        {routines.map((r) => (
+          <li key={r.id}>
+            {r.activity} - {r.duration}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
