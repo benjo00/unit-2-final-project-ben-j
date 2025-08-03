1689852577326c674d72bc0395da2b6c1b42951f
benjo00 <130617538+benjo00@users.noreply.github.com>
2025-08-03 12:56:46 -0500

    add routine manager

---
+++ frontend/src/components/RoutineManager.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function RoutineManager() {
+  return (
+    <div className="routine-manager">
+      <h3>Add a New Routine</h3>
+      {}
+    </div>
+  );
+}
+
+export default RoutineManager;
