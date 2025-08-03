de61c9b7702daa4dd43cc9d57f6f09b12006f68e
benjo00 <130617538+benjo00@users.noreply.github.com>
2025-08-03 12:43:13 -0500

    add placeholder

---
+++ frontend/src/components/RoutineList.jsx
@@ -0,0 +1,15 @@
+// RoutineList.jsx
+import React from "react";
+
+// for now just show a placeholder. will add more once backend connects
+
+function RoutineList() {
+  return (
+    <div>
+      <h2>Routine List</h2>
+      <p>this will eventually show all routines from backend</p>
+    </div>
+  );
+}
+
+export default RoutineList;
