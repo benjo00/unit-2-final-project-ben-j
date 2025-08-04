9e814b2d9b88ba08507c855df743c7e4aeee4a51
benjo00 <130617538+benjo00@users.noreply.github.com>
2025-08-04 16:38:54 -0500

    add layout css for routine manager

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,38 +7,33 @@ import Ancient from './components/Ancient';
 import About from './components/About';
 import Footer from './components/footer';
 import Header from './components/Header';
+import RoutineList from './components/RoutineList';
 
 function App() {
   return (
     <Router basename="/project-final-unit-1-ben-j">
-      <div className="App">
-        <nav id="links">
-          <Link to="/">Home</Link> { " | " }
-          <Link to="/modern">Modern</Link> { " | " }
-          <Link to="/modern/ancient">Ancient</Link> { " | " }
+      <div className='App'>
+        <nav id='links'>
+          <Link to="/">Home</Link>{" | "}
+          <Link to="/modern">Modern</Link>{" | "}
+          <Link to="/modern/ancient">Ancient</Link>{" | "}
           <Link to="/modern/ancient/about">About</Link>
         </nav>
-
-        <div className="content">
+        <div className='content'>
           <Header />
-
           <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <Home />
-                  <hr />
-                  <RoutineManager />
-                  <hr />
-                </>
-              }
-            />
+            <Route path="/" element={
+              <>
+                <Home />
+                <h2>Add a New Routine</h2> {}
+                <RoutineManager />
+                <RoutineList />
+              </>
+            } />
             <Route path="/modern" element={<Modern />} />
             <Route path="/modern/ancient" element={<Ancient />} />
             <Route path="/modern/ancient/about" element={<About />} />
           </Routes>
-
           <Footer />
         </div>
       </div>
